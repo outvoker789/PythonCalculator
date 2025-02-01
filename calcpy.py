f71cc3728ab627e5697589035495918e2d62075b
outvoker789 <outvoker789>
2025-02-01 12:01:38 +0200

    теперь всё в класе

--- calcpy.py
+++ calcpy.py
@@ -1,37 +1,27 @@
 class Calculator:
-
-  def addition():
-    amount_one = int(input("Введите первое значение суммы.\n - "))
-    amount_two = int(input("Введите второе значение суммы.\n - "))
-    print(f"Сумма: {amount_one + amount_two}")
-
-  def subtraction():
-    subtrn_one = int(input("Введите уменьшаемое значение.\n - "))
-    subtrn_two = int(input("Введите вычитаемое значение.\n - "))
-    print(f"Разность: {subtrn_one - subtrn_two}")
-
-  def multiplication():
-    factor_one = int(input("Введите первый множитель.\n - "))
-    factor_two = int(input("Введите второй множитель.\n - "))
-    print(f"Произведение: {factor_one * factor_two}")
-
-  def division():
-    divider_one = int(input("Введите делимое значение.\n - "))
-    divider_two = int(input("Введите делитель.\n - "))
-    print(f"Частное: {divider_one / divider_two}")
+  def __init__(self, mat_action):
+    self.mat_action = mat_action
+    if mat_action == "Сложение":
+      amount_one = int(input("Введите первое значение суммы.\n - "))
+      amount_two = int(input("Введите второе значение суммы.\n - "))
+      print(f"Сумма: {amount_one + amount_two}")
+    elif mat_action == "Вычитание":
+      subtrn_one = int(input("Введите уменьшаемое значение.\n - "))
+      subtrn_two = int(input("Введите вычитаемое значение.\n - "))
+      print(f"Разность: {subtrn_one - subtrn_two}")
+    elif mat_action == "Умножение":
+      factor_one = int(input("Введите первый множитель.\n - "))
+      factor_two = int(input("Введите второй множитель.\n - "))
+      print(f"Произведение: {factor_one * factor_two}")
+    elif mat_action == "Деление":
+      divider_one = int(input("Введите делимое значение.\n - "))
+      divider_two = int(input("Введите делитель.\n - "))
+      print(f"Частное: {divider_one / divider_two}")
+    else:
+      print("Команда не разпознана.")
 
 while True:
   print("Калькулятор.")
-  mat_action = input('Введите действие: "Сложение", "Вычитание", "Умножение", "Деление"\n - ')
-  if mat_action == "Сложение" or "Вычитание" or "Умножение" or "Деление":
-    if mat_action == "Сложение":
-      Calculator.addition()
-    elif mat_action == "Вычитание":
-      Calculator.subtraction()
-    elif mat_action == "Умножение":
-      Calculator.multiplication()
-    elif mat_action == "Деление":
-      Calculator.division()
-  else:
-    print("Команда не разпознана.")
+  classcalc = Calculator(input('Введите действие: "Сложение", "Вычитание", "Умножение", "Деление"\n - '))
+
 
