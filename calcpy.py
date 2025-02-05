18fc533ed9acf2a1ed540e14330e9aff6e2b9f68
outvoker789 <outvoker789>
2025-02-05 10:01:41 +0200

    Исправление ошибок

--- calcpy.py
+++ calcpy.py
@@ -15,40 +15,64 @@ print(expression)
 
 prior_oper = {"+":1,"-":1,"*":2,"/":2} 
 
+#Алгоритм создания обратной польской записи из инфиксной записи
 #Цикл перебора всех елементов математического выражения
 for element in expression:
-#Условие, если елемент это число то сразу добавить его в выходную строку
-    if element.isnumeric():
+#Перевод числа в флоат, если число оператор то выдаст ошибку и пойдёт дальше
+    try:
+        float(element)
         output_string.append(element)
+    except ValueError:    
 #Условие, если елемент это оператор
-    elif element in prior_oper:
+        if element in prior_oper:
 #Цикл который достаёт елементы с стека в выходную строку с условием, либо если стек пустой добавляет в него елемент
-        while operator_stack and operator_stack[-1] != "(" and prior_oper.get(operator_stack[-1], 0) >= prior_oper[element]:
-            output_string.append(operator_stack.pop())
-        operator_stack.append(element)
-    elif element == ")":
-        while operator_stack and operator_stack[-1] != "(":
-            output_string.append(operator_stack.pop())
-        operator_stack.pop()
-    elif element == "(":
-        operator_stack.append(element)
+            while operator_stack and operator_stack[-1] != "(" and prior_oper.get(operator_stack[-1], 0) >= prior_oper[element]:
+                output_string.append(operator_stack.pop())
+            operator_stack.append(element)
+#Условие, если елемент это закрывающая скобка
+        elif element == ")":
+#Добавляет элемент из стека в выходную строку, пока верхний элемент не является открывающей скобкой  
+            while operator_stack and operator_stack[-1] != "(":
+                output_string.append(operator_stack.pop())
+#Если в стеке осталась только скобка, удаляет её
+            operator_stack.pop()
+#Условие, если елемент это закрывающая скобка, то добавляем елемент в стек
+        elif element == "(":
+            operator_stack.append(element)
+
+#Добавляем все оставшиеся елементы из стека в выходную строку
 while operator_stack:
     output_string.append(operator_stack.pop())
     
+#Создание стека обратной польской записи
 reverse_polish = []
 
+#Алгоритм решения обратной польской записи
+#Перебираем все елементы в выходной строке из польской записи
 for term in output_string:
-    if term.isnumeric():
+#Условие если елемент это число то добавляем его в стек ОПЗ
+    try:
+        float(term)
         reverse_polish.append(term)
-    elif len(reverse_polish) >= 2:
-        b = reverse_polish.pop()
-        a = reverse_polish.pop()
-        if term == "+":
-            reverse_polish.append(float(a) + float(b))
-        elif term == "-":
-            reverse_polish.append(float(a) - float(b))
-        elif term == "*":
-            reverse_polish.append(float(a) * float(b))
-        elif term == "/":
-            reverse_polish.append(float(a) / float(b))
+    except ValueError:
+#Если нет то считаем количество елементов в стеке, если больше или равно 2, берём оттуда 2 числа и выполняем соотвествующее решение 
+        if len(reverse_polish) >= 2:
+            b = reverse_polish.pop()
+            a = reverse_polish.pop()
+            if term == "+":
+                reverse_polish.append(a + b)
+            elif term == "-":
+                reverse_polish.append(a - b)
+            elif term == "*":
+                reverse_polish.append(a * b)
+            
+            elif term == "/":
+                try:
+                    reverse_polish.append(a) / b)
+                except ZeroDivisionError:
+                    print("Я тебя на ноль помножу мудила")
+                    exit()
+
+
+#Выводим конечное решение в стеке
 print(reverse_polish)
